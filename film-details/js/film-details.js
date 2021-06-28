1ecc664978a815dd0e8f55954a88943025f27449
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-06-28 15:15:36 +0400

    feat: added input to search films

--- film-details/js/film-details.js
+++ film-details/js/film-details.js
@@ -4,18 +4,34 @@ let director = document.getElementById("directorArea");
 let producer = document.getElementById("producerArea");
 let releaseDate = document.getElementById("releaseDateArea");
 let areas = document.querySelectorAll(".areaClass");
-console.log(areas);
+let searchInput = document.getElementById("searchInputId");
+let searchBtn = document.getElementById("searchBtnId")
+// console.log(areas);
+console.log(searchInput.value);
 
-function getFilmDetails () {
-    fetch(`https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe`)
-        .then((response) => response.json())
-        .then((response) => {
-            title.innerText = response.title;
-            description.innerText = response.description;
-            director.innerText = response.director;
-            producer.innerText = response.producer;
-            releaseDate.innerText = response.release_date;
+searchBtn.addEventListener("click",function () {
+    // fetch(`https://ghibliapi.herokuapp.com/films/${searchInput.value}`)
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //         title.innerText = response.title;
+    //         description.innerText = response.description;
+    //         director.innerText = response.director;
+    //         producer.innerText = response.producer;
+    //         releaseDate.innerText = response.release_date;
+    //     })
+    fetch(`https://ghibliapi.herokuapp.com/films/`)
+        .then(response => response.json())
+        .then(response => {
+            return response.filter((film, index) => film.title.toLowerCase() === searchInput.value.toLowerCase())
         })
-}
+        .then(response => {
+            console.log(response);
+            title.innerText = response[0].title;
+            description.innerText = response[0].description;
+            director.innerText = response[0].director;
+            producer.innerText = response[0].producer;
+            releaseDate.innerText = response[0].release_date;
+        })
+})
 
-getFilmDetails();
+// getFilmDetails();
