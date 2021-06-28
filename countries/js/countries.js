fcc0bae3a785bac987f14e8f291d0429b586184a
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-06-28 17:56:35 +0400

    fix: fixed the showing section for countries

--- countries/js/countries.js
+++ countries/js/countries.js
@@ -7,16 +7,25 @@ allCountriesBtn.addEventListener("click", function(){
     fetch(`https://restcountries.eu/rest/v2/all`)
         .then(response => response.json())
         .then(response => {
-            // console.log(response)
+            console.log(response)
+            countriesListSection.textContent = "";
             response.forEach((country) => {
                 let row = document.createElement("p");
                 row.innerText = country.name;
                 countriesListSection.appendChild(row);
+                row.setAttribute("class", "countryName");
                 let flag = document.createElement("img");
                 flag.src = country.flag;
                 flag.width = 200;
                 flag.height = 150;
+                flag.setAttribute("class", "countryFlag");
                 countriesListSection.appendChild(flag);
+                row.addEventListener("click", function () {
+                    console.log("ok")
+                })
+                flag.addEventListener("click", function (event) {
+                    console.log(event.target)
+                })
             });
         });
 });
@@ -26,15 +35,24 @@ countrySearchBtn.addEventListener("click", function () {
         .then(response => response.json())
         .then(response => {
             response.filter((country) => {
-                if (country.name === countrySearchInput.value) {
+                if (country.name.toLowerCase() === countrySearchInput.value.toLowerCase()) {
+                    countriesListSection.textContent = "";
                     let row = document.createElement("p");
+                    row.setAttribute("class", "countryName");
                     row.innerText = country.name;
                     countriesListSection.appendChild(row);
                     let flag = document.createElement("img");
                     flag.src = country.flag;
                     flag.width = 200;
                     flag.height = 150;
+                    flag.setAttribute("class", "countryFlag");
                     countriesListSection.appendChild(flag);
+                    row.addEventListener("click", function () {
+                        console.log("ok")
+                    })
+                    flag.addEventListener("click", function () {
+                        console.log("ok")
+                    })
                 }
             })
         })
